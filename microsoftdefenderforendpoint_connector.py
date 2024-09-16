114e28ac18c493101a4a26fcc091ddec28ae48c7
grokas <grokas@splunk.com>
2024-09-16 11:57:31 -0700

    PAPP-34606 get investigation uri action

--- microsoftdefenderforendpoint_connector.py
+++ microsoftdefenderforendpoint_connector.py
@@ -2487,6 +2487,35 @@ class WindowsDefenderAtpConnector(BaseConnector):
 
         return action_result.set_status(phantom.APP_SUCCESS)
 
+    def _handle_get_investigation_uri(self, param):
+        """ This function retrieves a URI for downloading an investigation package by its action ID.
+
+        :param param: Dictionary of input parameters
+        :return: status(phantom.APP_SUCCESS/phantom.APP_ERROR)
+        """
+
+        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        action_id = param.get("action_id")
+
+        if not action_id:
+            return action_result.set_status(phantom.APP_ERROR, "Missing required parameter: action_id")
+
+        endpoint = "{0}{1}".format(self._graph_url, DEFENDER_GET_INVESTIGATION_URI.format(action_id=action_id))
+
+        ret_val, response = self._update_request(endpoint=endpoint, action_result=action_result, method="get")
+
+        if phantom.is_fail(ret_val):
+            return action_result.get_status()
+
+        action_result.add_data(response.get('value', "No URI found for the specified action"))
+
+        summary = action_result.update_summary({})
+        summary['action_taken'] = "Retrieved Investigation URI"
+
+        return action_result.set_status(phantom.APP_SUCCESS)
+
     def _handle_get_device_details(self, param):
         """ This function retrieves details for multiple devices by their device IDs.
 
